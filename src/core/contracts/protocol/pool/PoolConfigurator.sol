ba8a9a6a16200c7d746acd7da7ba50913d349895
Harsh Pandey <harshsatishpandey@gmail.com>
2024-04-10 21:52:14 +0530

    test: setLiquidationGracePeriodReserve

--- src/core/contracts/protocol/pool/PoolConfigurator.sol
+++ src/core/contracts/protocol/pool/PoolConfigurator.sol
@@ -261,7 +261,6 @@ abstract contract PoolConfigurator is VersionedInitializable, IPoolConfigurator 
     bool paused,
     uint40 gracePeriod
   ) public override onlyEmergencyOrPoolAdmin {
-    // Only setting grace period if the transition is paused -> unpaused
     if (!paused && gracePeriod != 0) {
       require(gracePeriod <= MAX_GRACE_PERIOD, Errors.INVALID_GRACE_PERIOD);
 
