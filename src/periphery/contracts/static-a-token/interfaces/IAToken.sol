3139ee5f1c35d31576f693c0b0cceeb4a2893e51
Harsh Pandey <harshsatishpandey@gmail.com>
2024-07-09 17:37:50 +0530

    feat: add static a token contracts

---
+++ src/periphery/contracts/static-a-token/interfaces/IAToken.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+interface IAToken {
+  function POOL() external view returns (address);
+
+  function getIncentivesController() external view returns (address);
+
+  function UNDERLYING_ASSET_ADDRESS() external view returns (address);
+
+  /**
+   * @notice Returns the scaled total supply of the scaled balance token. Represents sum(debt/index)
+   * @return The scaled total supply
+   */
+  function scaledTotalSupply() external view returns (uint256);
+}
