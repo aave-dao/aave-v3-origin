5d882805cec7ec7646c3574b4f0404c63a2962c0
sakulstra <lukasstrassel@googlemail.com>
2024-08-09 14:35:34 +0200

    fix: use oz

--- src/periphery/contracts/static-a-token/StataOracle.sol
+++ src/periphery/contracts/static-a-token/StataOracle.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.10;
 
+import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import {IPool} from '../../../core/contracts/interfaces/IPool.sol';
 import {IPoolAddressesProvider} from '../../../core/contracts/interfaces/IPoolAddressesProvider.sol';
 import {IAaveOracle} from '../../../core/contracts/interfaces/IAaveOracle.sol';
 import {IStataOracle} from './interfaces/IStataOracle.sol';
-import {IERC4626} from './interfaces/IERC4626.sol';
 
 /**
  * @title StataOracle
