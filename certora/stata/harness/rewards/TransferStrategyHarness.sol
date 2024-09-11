ce9735dd69e11fb1e3d107e3d119ec45ff0404d0
sakulstra <lukasstrassel@googlemail.com>
2024-09-11 08:40:34 +0200

    fix: lint certora files

--- certora/stata/harness/rewards/TransferStrategyHarness.sol
+++ certora/stata/harness/rewards/TransferStrategyHarness.sol
@@ -1,22 +1,23 @@
-
 pragma solidity ^0.8.10;
 
 import {IERC20} from 'aave-v3-core/contracts/dependencies/openzeppelin/contracts/IERC20.sol';
 import {TransferStrategyBase} from 'aave-v3-periphery/contracts/rewards/transfer-strategies/TransferStrategyBase.sol';
 
-contract TransferStrategyHarness is TransferStrategyBase{
+contract TransferStrategyHarness is TransferStrategyBase {
+  constructor(
+    address incentivesController,
+    address rewardsAdmin
+  ) TransferStrategyBase(incentivesController, rewardsAdmin) {}
 
-constructor(address incentivesController, address rewardsAdmin) TransferStrategyBase(incentivesController, rewardsAdmin) {}
+  IERC20 public REWARD;
 
-    IERC20 public REWARD;
-
-    // executes the actual transfer of the reward to the receiver
-    function performTransfer(
-        address to,
-        address reward,
-        uint256 amount
-    ) external override(TransferStrategyBase) returns (bool){
-        require(reward == address(REWARD));
-        return REWARD.transfer(to, amount);
-    }
+  // executes the actual transfer of the reward to the receiver
+  function performTransfer(
+    address to,
+    address reward,
+    uint256 amount
+  ) external override(TransferStrategyBase) returns (bool) {
+    require(reward == address(REWARD));
+    return REWARD.transfer(to, amount);
+  }
 }
--- certora/stata/harness/tokens/DummyERC20_rewardToken.sol
+++ certora/stata/harness/tokens/DummyERC20_rewardToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
-import "./DummyERC20Impl.sol";
+import './DummyERC20Impl.sol';
 
 contract DummyERC20_rewardToken is DummyERC20Impl {}
