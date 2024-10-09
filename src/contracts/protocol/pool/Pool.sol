181fcd32eb02e83f79902f010153b29136fa00cb
Lukas <lukasstrassel@googlemail.com>
2024-10-09 11:42:19 +0200

    Update src/contracts/protocol/pool/Pool.sol
    Co-authored-by: Ernesto Boado <ebdmrr@gmail.com>

--- src/contracts/protocol/pool/Pool.sol
+++ src/contracts/protocol/pool/Pool.sol
@@ -452,7 +452,7 @@ abstract contract Pool is VersionedInitializable, PoolStorage, IPool {
     res.isolationModeTotalDebt = reserve.isolationModeTotalDebt;
     // This is a temporary workaround for integrations that are broken by Aave 3.2
     // While the new pool data provider is backward compatible, some integrations hard-code an old implementation
-    // To allow them to unlock the funds, the pool address provider is setting a stable debt token, so balanceOf() and totalSupply() will return zero instead of reverting
+    // To allow them to not have any infrastructural blocker, a mock must be configured in the Aave Pool Addresses Provider, returning zero on all required view methods, instead of reverting
     res.stableDebtTokenAddress = ADDRESSES_PROVIDER.getAddress(bytes32('MOCK_STABLE_DEBT'));
     return res;
   }
