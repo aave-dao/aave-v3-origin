794aaf1feaa4ca3dc7e0657bc4d01523df9b8f72
Lukas <lukasstrassel@googlemail.com>
2024-12-13 09:32:11 +0100

    fix: update gas tests

--- tests/gas/Testhelpers.sol
+++ tests/gas/Testhelpers.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import {IERC20} from 'openzeppelin-contracts/contracts/interfaces/IERC20.sol';
+import {IPriceOracleGetter} from '../../src/contracts/interfaces/IPriceOracleGetter.sol';
 import {TestnetProcedures} from '../utils/TestnetProcedures.sol';
 
 contract Testhelpers is TestnetProcedures {
@@ -23,12 +24,30 @@ contract Testhelpers is TestnetProcedures {
     _skip(100); // skip some blocks to allow interest to accrue & the block to be not cached
   }
 
+  /**
+   * Supplies the specified amount of asset to the reserve.
+   */
   function _supplyOnReserve(address user, uint256 amount, address asset) internal {
     vm.startPrank(user);
     deal(asset, user, amount);
     IERC20(asset).approve(report.poolProxy, amount);
     contracts.poolProxy.supply(asset, amount, user, 0);
     vm.stopPrank();
+  }
+
+  // assumes that the caller has at least one unit of collateralAsset that is not the borrowAsset
+  function _borrowArbitraryAmount(address borrower, uint256 amount, address asset) internal {
+    // set the oracle price of the borrow asset to 0
+    vm.mockCall(
+      address(contracts.aaveOracle),
+      abi.encodeWithSelector(IPriceOracleGetter.getAssetPrice.selector, address(asset)),
+      abi.encode(0)
+    );
+    // borrow the full emount of the asset
+    vm.prank(borrower);
+    contracts.poolProxy.borrow(asset, amount, 2, 0, borrower);
+    // revert the oracle price
+    vm.clearMockedCalls();
   }
 
   /**
