68b4639baff7a37cdb859254ab99eb238c0a6c0b
Lukas <lukasstrassel@googlemail.com>
2025-03-05 11:00:34 +0100

    fix: lint

--- src/contracts/misc/EmptyImplementation.sol
+++ src/contracts/misc/EmptyImplementation.sol
@@ -5,6 +5,4 @@ pragma solidity ^0.8.0;
  * @notice This contract is an intentionally empty implementation.
  * It's to be used for proxies that should be initialized with zero functionality.
  */
-contract EmptyImplementation {
-
-}
+contract EmptyImplementation {}
