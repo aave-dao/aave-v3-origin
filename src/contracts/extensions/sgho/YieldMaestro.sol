9ca84191e06e85a812e4f0c7781095d60e24de17
luigy-lemon <luigy-lemon@protonmail.com>
2025-04-02 22:29:45 +0100

    feat: init commit

---
+++ src/contracts/extensions/sgho/YieldMaestro.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: agpl-3
+pragma solidity ^0.8.19;
+
+import {Initializable} from 'openzeppelin-contracts/contracts/proxy/utils/Initializable.sol';
+import 'openzeppelin-contracts/contracts/interfaces/IERC4626.sol';
+import {IERC20Permit} from 'openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Permit.sol';
+import {IWXDAI} from './interfaces/IWXDAI.sol';
+import {sGHO} from './sGHO.sol';
+
+contract YieldMaestro is Initializable {
+  IERC20 public constant gho;
+  address public constant sGHO;
+
+  uint256 public lastClaimTimestamp;
+  uint256 public targetRate;
+
+  uint256 internal constant RATE_PRECISION = 1e8;
+  uint256 internal constant ONE_YEAR = 365 days;
+
+  event Claimed(uint256 indexed amount);
+
+  constructor(address _sGho, address aclm) {
+    sGHO = _sGho;
+    claimer = msg.sender;
+  }
+
+  modifier isInitialized() {
+    require(_getInitializedVersion() > 0, 'Not Initialized');
+    _;
+  }
+
+  modifier onlyYieldManagerOrAdmin() {
+    if( msg.sender != aclManager.isYieldManager() && msg.sender != aclManager.isPoolAdmin(), 'Not Yield Manager'){
+        revert onlyAdmin();
+    }
+    _;
+  }
+
+  modifier onlyAdmin() {
+    if (msg.sender != aclManager.isPoolAdmin()) {
+      revert onlyAdmin();
+    }
+    _;
+  }
+
+  /**
+   * @dev Initialize receiver, require minimum balance to not set a dripRate of 0
+   */
+  function initialize() public payable initializer {
+    lastClaimTimestamp = block.timestamp;
+    setTargetRate(0);
+  }
+
+  function claimSavings() public isInitialized returns (uint256 claimed) {
+    // if APY is 0 skip it
+    if (apy > 0) {
+      (unclaimed) = _calculateUnclaimed();
+      gho.transfer(sGHO, unclaimed);
+      claimed = unclaimed;
+      emit Claimed(claimed);
+    }
+    lastClaimTimestamp = block.timestamp;
+
+  }
+
+  function _calculateUnclaimed() public view returns (uint256) {
+    // Calculate the time elapsed since the last claim
+    uint256 elapsedTime = block.timestamp - lastClaimTimestamp;
+    uint256 vaultAssets = IERC4626(sGHO).totalAssets();
+
+    // Calculate unclaimed rewards based on APY
+    uint256 unclaimedRewards = (vaultAssets * targetRate * elapsedTime) / (RATE_PRECISION * ONE_YEAR);
+    return unclaimedRewards;
+}
+
+  /**
+   * @dev Preview how much would be claimable
+   */
+  function previewClaimable() external view returns (uint256 claimable) {
+    claimable = _calculateUnclaimed();  
+  }
+
+  /**
+   * @dev Informs about approximate sDAI vault APR based on incoming bridged interest and vault deposits
+   * @return amount of interest collected per year divided by amount of current deposits in vault
+   */
+  function vaultAPR() external view returns (uint256) {
+    uint256 deposits = sDAI.totalAssets();
+    uint256 annualYield = (dripRate * 365 days);
+    return (1 ether * annualYield) / deposits;
+  }
+
+  /**
+   * @dev set new target rate in APR, such that a target rate of 10% should have input 1000
+   * @param newRate 
+   * @return amount of interest collected per year divided by amount of current deposits in vault
+   */
+  function setTargetRate(uint256 newRate) external onlyYieldManagerOrAdmin {
+    // offset by 2 decimals relative to RATE_PRECISION
+    targetRate = newRate * 1e6
+  }
+
+  /// @inheritdoc IRescuableBase
+  function rescueERC20(address erc20Token, address to, uint256 amount) external onlyAdmin() {
+    uint256 max = IERC20(erc20Token).balanceOf(address(this));
+    amount = max > amount ? amount : max;
+    IERC20(erc20Token).safeTransfer(to, amount);
+    emit ERC20Rescued(msg.sender, erc20Token, to, amount);
+  }
+
+  receive() external payable {
+    revert('No ETH allowed');
+  }
+}
---
+++ src/contracts/extensions/sgho/sGHO.sol
@@ -0,0 +1,268 @@
+// SPDX-License-Identifier: agpl-3
+pragma solidity ^0.8.19;
+
+import {ERC4626} from 'openzeppelin-contracts/contracts/token/ERC20/extensions/ERC4626.sol';
+import {IERC20Permit} from 'openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Permit.sol';
+import {ECDSA} from 'openzeppelin/utils/cryptography/ECDSA.sol';
+import {EIP712} from 'openzeppelin/utils/cryptography/EIP712.sol';
+import {Nonces} from 'openzeppelin/utils/Nonces.sol';
+
+interface IERC1271 {
+  function isValidSignature(bytes32, bytes memory) external view returns (bytes4);
+}
+
+contract sGHO is ERC4626, IERC20Permit, EIP712, Nonces {
+  IERC20 public constant gho;
+  address public constant YIELD_MAESTRO;
+  uint256 internal internalTotalAssets;
+
+  // --- EIP712 niceties ---
+  uint256 public immutable deploymentChainId;
+  bytes32 private immutable _DOMAIN_SEPARATOR;
+  bytes32 public constant PERMIT_TYPEHASH =
+    keccak256('Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)');
+  string public constant VERSION = '1';
+
+  /**
+   * @dev Permit deadline has expired.
+   */
+  error ERC2612ExpiredSignature(uint256 deadline);
+
+  /**
+   * @dev Mismatched signature.
+   */
+  error ERC2612InvalidSigner(address signer, address owner);
+
+  /**
+   * @dev Set the underlying asset contract. This must be an ERC20-compatible contract (ERC20 or ERC777).
+   * @param _gho The address of the GHO token contract.
+   * @param _yieldMaestro The address of the Yield Maestro contract.
+   */
+  constructor(
+    address _gho,
+    address _yieldMaestro
+  ) ERC20('sGHO', 'sGHO') ERC4626(IERC20(_gho)) EIP712('sGHO', '1') {
+    deploymentChainId = block.chainid;
+    _DOMAIN_SEPARATOR = _calculateDomainSeparator(block.chainid);
+    gho = _gho;
+    YIELD_MAESTRO = _yieldMaestro;
+  }
+
+  receive() external payable {
+    revert('No ETH allowed');
+  }
+
+  // --- Approve by signature ---
+
+  function _isValidSignature(
+    address signer,
+    bytes32 digest,
+    bytes memory signature
+  ) internal view returns (bool) {
+    if (signature.length == 65) {
+      bytes32 r;
+      bytes32 s;
+      uint8 v;
+      assembly {
+        r := mload(add(signature, 0x20))
+        s := mload(add(signature, 0x40))
+        v := byte(0, mload(add(signature, 0x60)))
+      }
+      if (signer == ecrecover(digest, v, r, s)) {
+        return true;
+      }
+    }
+
+    (bool success, bytes memory result) = signer.staticcall(
+      abi.encodeWithSelector(IERC1271.isValidSignature.selector, digest, signature)
+    );
+    return (success &&
+      result.length == 32 &&
+      abi.decode(result, (bytes4)) == IERC1271.isValidSignature.selector);
+  }
+
+  function permit(
+    address owner,
+    address spender,
+    uint256 value,
+    uint256 deadline,
+    bytes memory signature
+  ) public {
+    require(block.timestamp <= deadline, 'SavingsXDai/permit-expired');
+    require(owner != address(0), 'SavingsXDai/invalid-owner');
+
+    uint256 nonce = _useNonce(owner);
+
+    bytes32 digest = keccak256(
+      abi.encodePacked(
+        '\x19\x01',
+        block.chainid == deploymentChainId
+          ? _DOMAIN_SEPARATOR
+          : _calculateDomainSeparator(block.chainid),
+        keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonce, deadline))
+      )
+    );
+
+    require(_isValidSignature(owner, digest, signature), 'SavingsXDai/invalid-permit');
+
+    _approve(owner, spender, value);
+    emit Approval(owner, spender, value);
+  }
+
+  function permit(
+    address owner,
+    address spender,
+    uint256 value,
+    uint256 deadline,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  ) external {
+    permit(owner, spender, value, deadline, abi.encodePacked(r, s, v));
+  }
+  /**
+   * @dev See {IERC20Permit-nonces}.
+   */
+
+  function nonces(
+    address owner
+  ) public view virtual override(IERC20Permit, Nonces) returns (uint256) {
+    return super.nonces(owner);
+  }
+
+  /**
+   * @dev See {IERC20Permit-DOMAIN_SEPARATOR}.
+   */
+  function DOMAIN_SEPARATOR() external view virtual returns (bytes32) {
+    return _domainSeparatorV4();
+  }
+
+  function _calculateDomainSeparator(uint256 chainId) private view returns (bytes32) {
+    return
+      keccak256(
+        abi.encode(
+          keccak256(
+            'EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'
+          ),
+          keccak256(bytes(name())),
+          keccak256(bytes(VERSION)),
+          chainId,
+          address(this)
+        )
+      );
+  }
+
+  // --- ERC4626 Logic ---
+
+  function deposit(uint256 assets, address receiver) public override returns (uint256) {
+    uint256 maxAssets = maxDeposit(receiver);
+    if (assets > maxAssets) {
+      revert ERC4626ExceededMaxDeposit(receiver, assets, maxAssets);
+    }
+
+    _updateVault(assets, true);
+
+    uint256 shares = previewDeposit(assets);
+    _deposit(_msgSender(), receiver, assets, shares);
+
+    return shares;
+  }
+
+  function mint(uint256 shares, address receiver) public override returns (uint256) {
+    uint256 maxShares = maxMint(receiver);
+    if (shares > maxShares) {
+      revert ERC4626ExceededMaxMint(receiver, shares, maxShares);
+    }
+
+    _updateVault(assets, true);
+
+    uint256 assets = previewMint(shares);
+    _deposit(_msgSender(), receiver, assets, shares);
+
+    return assets;
+  }
+
+  /** @dev See {IERC4626-withdraw}. */
+  function withdraw(
+    uint256 assets,
+    address receiver,
+    address owner
+  ) public override returns (uint256) {
+    uint256 maxAssets = maxWithdraw(owner);
+    if (assets > maxAssets) {
+      revert ERC4626ExceededMaxWithdraw(owner, assets, maxAssets);
+    }
+
+    uint256 shares = previewWithdraw(assets);
+    _withdraw(_msgSender(), receiver, owner, assets, shares);
+
+    _updateVault(assets, false);
+
+    return shares;
+  }
+
+  /** @dev See {IERC4626-redeem}. */
+  function redeem(
+    uint256 shares,
+    address receiver,
+    address owner
+  ) public override returns (uint256) {
+    uint256 maxShares = maxRedeem(owner);
+    if (shares > maxShares) {
+      revert ERC4626ExceededMaxRedeem(owner, shares, maxShares);
+    }
+
+    uint256 assets = previewRedeem(shares);
+    _withdraw(_msgSender(), receiver, owner, assets, shares);
+
+    _updateVault(assets, false);
+
+    return assets;
+  }
+
+  /** @dev See {IERC4626-totalAssets}. */
+  function totalAssets() public view returns (uint256) {
+    return internalTotalAssets;
+  }
+
+  /**
+   * @dev Update the internal total assets of the vault.
+   * This function is called when assets are deposited or withdrawn.
+   * It also claims the savings from the Yield Maestro if the last update was more than 100 seconds ago.
+   * @param assets The amount of assets to update.
+   * @param assetIncrease A boolean indicating whether the assets are being increased or decreased.
+   */
+  function _updateVault(uint256 assets, bool assetIncrease) internal {
+    uint256 currentTime = block.timestamp;
+    uint256 lastupdate = lastUpdate;
+    uint256 claimed;
+    if (currentTime > lastupdate + 100) {
+      claimed = IYieldMaestro(YIELD_MAESTRO).claimSavings();
+      internalTotalAssets += claimed;
+      lastUpdate = currentTime;
+    }
+
+    if (assetIncrease) {
+      internalTotalAssets += assets;
+    } else {
+      internalTotalAssets -= assets;
+    }
+    //verify asset balance changed as expected, if it doesn't match, then set internal balances to asset balance
+    uint256 balance = gho.balanceOf(address(this));
+    // In case of assetIncrease transfer has not happened yet. Therefore we need to discount the amount of assets
+    uint256 expectedBalance = (assetIncrease) ? internalTotalAssets - assets : internalTotalAssets;
+    if (balance < expectedBalance) {
+      internalTotalAssets = balance;
+    }
+  }
+
+  /**
+   * @dev Transfer any excess GHO tokens to the Yield Maestro.
+   */
+  function takeDonated() external {
+    uint256 balance = gho.balanceOf(address(this));
+    if (balance > totalAssets()) {
+      gho.transfer(YIELD_MAESTRO, balance - totalAssets());
+    }
+  }
+}
